58e7e83f5073b24cd4c80ebbee65b10f3f2e7337
Cole Fire <ColedFeuer@gmail.com>
2017-08-06 10:13:56 -0700

    color continents

--- gapminder-plots.R
+++ gapminder-plots.R
@@ -1,3 +1,3 @@
 gapminder <- read.csv("data/gapminder-FiveYearData.csv")
 library(ggplot2)
-ggplot(gapminder, aes(x = gdpPercap, y = lifeExp)) + geom_point()
+ggplot(gapminder, aes(x = gdpPercap, y = lifeExp)) + geom_point(aes(color = continent))
