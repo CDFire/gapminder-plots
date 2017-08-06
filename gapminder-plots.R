6c05da3f280e45a02cefd8d0d02b7d1aa8ebc3a3
Cole Fire <ColedFeuer@gmail.com>
2017-08-06 11:58:45 -0700

    add japan graph

--- gapminder-plots.R
+++ gapminder-plots.R
@@ -1,3 +1,6 @@
 gapminder <- read.csv("data/gapminder-FiveYearData.csv")
 library(ggplot2)
 ggplot(gapminder, aes(x = gdpPercap, y = lifeExp)) + scale_x_log10() + geom_point(aes(color = continent)) + geom_line(aes(color = continent)) + geom_smooth(method = "lm", color = "purple")
+
+japan <- gapminder[gapminder$country == "Japan", ]
+ggplot(japan, aes(x = year, y = lifeExp)) + geom_line(color = "coral") + xlab("Life expectancy") + ylab("year")
